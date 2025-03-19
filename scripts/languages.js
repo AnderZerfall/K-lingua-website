4b199136deb25b13b192b4b69c9d266718579914
Fukuro <anya.androschuk@gmail.com>
2025-03-19 16:51:39 +0200

    fixes

--- scripts/languages.js
+++ scripts/languages.js
@@ -7,73 +7,80 @@ import { howWeWork } from "../languages/howWeWork.js";
 import { whoAreWe } from "../languages/whoAreWe.js";
 import { prices } from "../languages/prices.js";
 
-const langButtons = document.querySelectorAll('.header__lang-text');
-const CURRENT_LANG = 'k-lang';
+window.onload = () => {
+  setTimeout(() => {
+    const langButtons = document.querySelectorAll(".header__lang-text");
+    const CURRENT_LANG = "k-lang";
 
-const defineLang = (langButton = null) => {
-  let lang = sessionStorage.getItem(CURRENT_LANG) || "en";
+    const defineLang = (langButton = null) => {
+      let lang = sessionStorage.getItem(CURRENT_LANG) || "en";
 
-  if (langButton) {
-    lang = langButton.id;
-  }
+      if (langButton) {
+        lang = langButton.id;
+      }
 
-  currentLang = lang;
-  sessionStorage.setItem(CURRENT_LANG, currentLang);
+      currentLang = lang;
+      sessionStorage.setItem(CURRENT_LANG, currentLang);
 
-  langButtons.forEach(lan => lan.id === lang ? lan.classList.remove('current') : lan.classList.add('current'));
-}
-
-let currentLang = 'en';
-defineLang();
-
-const currentPathName = window.location.pathname;
-let currentText = { ...header, ...footer };
-
-const checkPageName = () => {
-  switch (currentPathName) {
-    case "/index.html":
-      currentText = { ...currentText, ...home };
-      break;
-    case "/Benefits.html":
-      currentText = { ...currentText, ...benefits };
-      break;
-    case "/Contact.html":
-      currentText = { ...currentText, ...contact };
-      break;
-    case "/Prices.html":
-      currentText = { ...currentText, ...prices };
-      break;
-    case "/HowWeWork.html":
-      currentText = { ...currentText, ...howWeWork };
-      break;
-    case "/WhoAreWe.html":
-      currentText = { ...currentText, ...whoAreWe };
-      break;
-    default:
-      break;
-  }
-};
-
-checkPageName();
-
-const changeLanguage = () => {
-  for (const key in currentText) {
-    let elements = document.querySelectorAll(`[data-ln=${key}]`);
-
-    if (elements.length) {
-      elements.forEach((elem) => {
-        elem.textContent = currentText[key][currentLang];
-        }
+      langButtons.forEach((lan) =>
+        lan.id === lang
+          ? lan.classList.remove("current")
+          : lan.classList.add("current")
       );
-    }
-  }
-};
+    };
 
-changeLanguage();
+    let currentLang = "en";
+    defineLang();
 
-langButtons.forEach(lang => {
-  lang.addEventListener('click', () => {
-    defineLang(lang);
+    const currentPathName = window.location.pathname;
+    let currentText = { ...header, ...footer };
+
+    const checkPageName = () => {
+      switch (currentPathName) {
+        case "/index.html":
+          currentText = { ...currentText, ...home };
+          break;
+        case "/Benefits.html":
+          currentText = { ...currentText, ...benefits };
+          break;
+        case "/Contact.html":
+          currentText = { ...currentText, ...contact };
+          break;
+        case "/Prices.html":
+          currentText = { ...currentText, ...prices };
+          break;
+        case "/HowWeWork.html":
+          currentText = { ...currentText, ...howWeWork };
+          break;
+        case "/WhoAreWe.html":
+          currentText = { ...currentText, ...whoAreWe };
+          break;
+        default:
+          break;
+      }
+    };
+
+    checkPageName();
+
+    const changeLanguage = () => {
+      for (const key in currentText) {
+        let elements = document.querySelectorAll(`[data-ln=${key}]`);
+
+        if (elements.length) {
+          elements.forEach((elem) => {
+            elem.textContent = currentText[key][currentLang];
+          });
+        }
+      }
+    };
+
     changeLanguage();
-  });
-});
+
+    langButtons.forEach((lang) => {
+      lang.addEventListener("click", () => {
+        defineLang(lang);
+        changeLanguage();
+      });
+    });
+  }, 100);
+}
